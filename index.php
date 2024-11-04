9e0c1359dcb506359a10447eee5fcc2e6f5b3d94
Josemacias354 <maciasbichillo1@gmail.com>
2024-11-04 13:08:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-    $user_agent = $_SERVER['HTTP_USER_AGENT'];
 function get_browser_name($user_agent)
 {
-    if(strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
-    elseif(strpos($user_agent, 'Edge')) return 'Edge';
-    elseif(strpos($user_agent, 'Chrome')) return 'Chrome';
-    elseif(strpos($user_agent, 'Safari')) return 'Safari';
-    elseif(strpos($user_agent, 'Firefox')) return 'Firefox';
+    if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
+    elseif (strpos($user_agent, 'Edge')) return 'Edge';
+    elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
+    elseif (strpos($user_agent, 'Safari')) return 'Safari';
+    elseif (strpos($user_agent, 'Firefox')) return 'Firefox';
+    elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'Internet Explorer';
 
+    return 'Other';
 }
-
-$browser = get_browser_name($_SERVER['HTTP_USER_AGENT']);
-echo $browser;
+// Usage:
+echo get_browser_name($_SERVER['HTTP_USER_AGENT']);
