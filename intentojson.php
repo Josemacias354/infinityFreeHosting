df1c44ee028b7b002140212663238eed702ccb01
Josemacias354 <maciasbichillo1@gmail.com>
2024-11-21 09:19:36 +0100

    Update intentojson.php

--- intentojson.php
+++ intentojson.php
@@ -2,3 +2,11 @@
 $data=$_POST['dades'];
 
 echo json_encode($data);
+
+echo "<h1>Formulari</h1>";
+$nom = $data['nom'];
+echo "<p><strong>Nom:</strong> $nom</p>";
+$edat = $data['edat'];
+echo "<p><strong>Edat:</strong> $edat</p>";
+$dni = $data['dni'];
+echo "<p><strong>DNI:</strong> $dni</p>";
