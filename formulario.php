c77e471fb0b986c2415a7735291937a1612b7fdf
Josemacias354 <maciasbichillo1@gmail.com>
2024-11-21 08:40:59 +0100

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -1,16 +1,12 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom = $_POST['nombre'];
-    $edat = $_POST['edad'];
-    $dni = $_POST['dni'];
 
-    if (is_numeric($nom) || is_numeric($dni)) {
-        echo "<p>El nom i el DNI no poden ser nombres</p>";
-    }
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $nom = $_GET['nom'];
+    $edat = $_GET['edat'];
+    $dni = $_GET['dni'];
 
     echo "<h1>Formulari</h1>";
     echo "<p><strong>Nom:</strong> $nom</p>";
     echo "<p><strong>Edat:</strong> $edat</p>";
     echo "<p><strong>DNI:</strong> $dni</p>";
 }
-?>
