c2bfa38f84320ca614a3bbf99c09601b56316de0
Josemacias354 <maciasbichillo1@gmail.com>
2024-11-21 08:59:43 +0100

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -9,4 +9,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<p><strong>Nom:</strong> $nom</p>";
     echo "<p><strong>Edat:</strong> $edat</p>";
     echo "<p><strong>DNI:</strong> $dni</p>";
+
+
+
+
+    echo "<h1>Formulari</h1>";
+    echo "<p>$nom</p>";
+    echo "<p>$edat</p>";
+    echo "<p>$dni</p>";
 }
+?>
