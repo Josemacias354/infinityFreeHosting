17a16967008df656ebc0a404454a3a98f4244511
Josemacias354 <maciasbichillo1@gmail.com>
2024-11-25 11:34:31 +0100

    Update inentoex3.php

--- inentoex3.php
+++ inentoex3.php
@@ -7,11 +7,14 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $dades = $_POST['dades'];    }
 
 $dades_json = json_decode($dades, true);
+
     echo "Nombre: $nom <br>";
     echo "DNI: $dni <br>";
     echo "Edad: $edat <br>";
+    echo "<br>";
 
-echo json_encode($dades_json);   //Segunda tentativa
+echo json_encode($dades_json);
+
     echo "Nombre: ".$dades_json['nom']."<br>";
     echo "Edad: ".$dades_json['edat']."<br>";
     echo "DNI: ".$dades_json['dni']."<br>";?>
